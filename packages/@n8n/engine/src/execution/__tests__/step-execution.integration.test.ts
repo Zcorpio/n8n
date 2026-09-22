00e6a12d7205a281649b26a1f6955c45025504ae
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2026-09-22 14:05:50 +0300

    test(engine): Use the response sender interface

--- packages/@n8n/engine/src/execution/__tests__/step-execution.integration.test.ts
+++ packages/@n8n/engine/src/execution/__tests__/step-execution.integration.test.ts
@@ -391,7 +391,7 @@ describe('step execution (integration)', () => {
 			orchestrationQueue,
 			{ v1StepExecutor: { execute } },
 			noopLifecycleEventPublisher,
-			new ExecutionResponseSender(noopResponseFrameSender),
+			noopExecutionResponseSender,
 		);
 
 		const executionId = generateId();
